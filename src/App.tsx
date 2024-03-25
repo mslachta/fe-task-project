cbbe3d78c874838d392f1cfe1ce4eb6410ed6b42
Martin Slachta <martinslachta@gmail.com>
2024-03-25 16:28:49 +0100

    Implement navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import "./App.css";
-
+import NavigationBar from "./components/NavigationBar";
 export default function App() {
   return (
     <>
-      
+      <NavigationBar />
     </>
   );
 }
--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -3,17 +3,17 @@ import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
 
 const navigation = [
-  { name: 'Dashboard', href: '#', current: true },
-  { name: 'Team', href: '#', current: false },
-  { name: 'Projects', href: '#', current: false },
-  { name: 'Calendar', href: '#', current: false },
+  { name: 'Dashboard', href: '/', current: true },
+  { name: 'Bookings', href: 'booking', current: false },
+  { name: 'FAQ', href: 'FAQ', current: false },
+
 ]
 
 function classNames(...classes:any[]) {
   return classes.filter(Boolean).join(' ')
 }
 
-export default function Example() {
+export default function NavigationBar() {
   return (
     <Disclosure as="nav" className="bg-gray-800">
       {({ open }) => (
